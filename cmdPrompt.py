1e82850d8f71bf5fa6cc10651d9d31b5c43e7d83
G. Assumani <167922912+Steam90902@users.noreply.github.com>
2025-09-13 18:14:13 +0100

    Finished with commands
    I finished up the commands and made sure it doesnt crash with wrong input to a suitable degree, Now all i need to do is move all the commands functions to another python file to freshen up the main file. :D

--- cmdPrompt.py
+++ cmdPrompt.py
@@ -14,6 +14,19 @@ print("\n")
 #os.rename([initial name], [changed name]) #renames a file
 #as.system(filename) #runs a file
 
+def cNextDir(subItem):
+    i = os.getcwd()
+    ko = i + "/" + subItem
+    return ko
+
+def alreadyHere(subItem):
+    if os.path.exists(subItem):
+        return True
+    else:
+        return False
+
+
+
 
 def cd(item): #opens or backs out of a dir
     x = item.split(" ", 1)
@@ -38,105 +51,146 @@ def ls(): #lists the current dir
     print(os.listdir())
 
 def search(item): #Just confirms if a file is there
-    if os.path.exists(item):
-        print(f"{item} is present")
-    else:
-        print(f"Sorry! {item} is not here!")
+    if item != "":
+        if os.path.exists(item):
+            print(f"{item} is present")
+        else:
+            print(f"Sorry! {item} is not here!")
 
-def run(item): #should run a file, currently not working
+def run(item): #should run a file or even a normal command
     x = item.split(" ", 1)
-    x = x[1]
-    os.system(x)
+    if len(x) == 2:
+        if shutil.which(x[1]) is None:
+            print(f"No executable found for command {x[1]}")
+        else:
+            os.system(x[1])
+    else:
+        print("No arguments found")
 
 def rm(item): #removes a file NOT a folder
     x = item.split(" ", 1)
-    n = x[1]
-    if os.path.exists(n):
-            os.remove(n)
-            print(f"{n} deleted")
+    if len(x) == 2:
+        n = x[1]
+        if os.path.exists(n):
+                conf = str(input(f"confirm you want to delete {n}? (y/n)"))
+                if conf == "yes" or conf == "y":
+                    os.remove(n)
+                    print(f"{n} deleted")
+        else:
+            print(f"{n} doesnt exist (maybe you got the name wrong?)")
     else:
-        print("That file doesnt exist (maybe you got the name wrong?)")
+        print("Not enough arguments")
 
 def touch(item): #creates a file 
-    conf = str(input(f"confirm you want to create a file? (y/n)"))
-    if conf == "yes" or conf == "y":
-        x = item.split(" ", 1)
-        x = x[1]
-        os.system("touch {}".format(x))
+    x = item.split(" ", 1)
+    if len(x) == 2 and alreadyHere(x[1]) == False:
+        conf = str(input(f"confirm you want to create a file? (y/n)"))
+        if conf == "yes" or conf == "y":
+            x = x[1]
+            os.system("touch {}".format(x))
+        else:
+            print(" ")
     else:
-        print(" ")
-
-def cNextDir(subItem):
-    i = os.getcwd()
-    ko = i + "/" + subItem
-    return ko
-
+        print("Not enough arguments")
+ 
 def mkdir(item): # creates a folder
     x = item.split(" ", 1)
-    if len(x) == 2:
-        x = x[1]
-        os.mkdir(x)
+    conf = str(input(f"confirm you want to create a folder? (y/n)"))
+    if conf == "yes" or conf == "y":
+        if len(x) == 2 and alreadyHere(x[1]) == False:
+            x = x[1]
+            os.mkdir(x)
+        elif alreadyHere("Untitled") == False and len(x) == 1:
+            os.mkdir("Untitled")
+        else:
+            print("Untitled already exists")
     else:
-        os.mkdir("Untitled")
+        print("That folder already exists")
 
 def mv(item): # moves files 
     x = item.split(" ", 1)
-    x = x[1]
-    os.system("mv {}".format(x))
-    
-def cp(item):
-    x = item.split(" ", 1)
-    x = x[1]
-    x = x.split(" -/- ", 1)
-    st = x.pop(0)
-    des = x.pop(0)
-    #print(st)
-    #print(des)
-    if "/home" in st and "/home" in des:
-        shutil.copy2(st, des)
-        print("Files have been copied")
+    if len(x) == 2:
+        x = x[1]
+        x = x.split(" -/- ", 1)
+        fail = "Usage: mv source -/- destination"
+        st = x.pop(0)
+        des = x.pop(0)
+        #print(st)
+        #print(des)
+        if os.path.exists(st) and os.path.exists(des):
+            os.replace(st, des)
+            print("Files have been moved")
+        else:
+            print(f"{fail} \n Maybe you got the path wrong?")
     else:
-        print("No paths detected")
+        print(f"{fail} \n Maybe you didnt put enough arguments?")
+    
+def cp(item): #copies files 
+    x = item.split(" ", 1)
+    fail = "Usage: cp source -/- destination"
+    if len(x) == 2:
+        x = x[1]
+        x = x.split(" -/- ", 1)
+        st = x.pop(0)
+        des = x.pop(0)
+        #print(st)
+        #print(des)
+        if os.path.exists(st) and os.path.exists(des):
+            shutil.copy2(st, des)
+            print("Files have been copied")
+        else:
+            print(f"{fail} \n Maybe you got the path wrong?")
+    else:
+        print(f"{fail} \n Maybe you didnt put enough arguments?")
 
-def rmdir(item):
+def rmdir(item): # removes folders
     x = item.split(" ", 1)
     n = x[1]
     if os.path.exists(n):
-        if len(os.listdir(cNextDir(n))) == 0:
-            os.rmdir(n)
-            print(f"{n} deleted")
-        else:
-            print(f"There may be some files inside")
+        conf = str(input(f"confirm you want to delete {n}? (y/n)"))
+        if conf == "yes" or conf == "y":
+            if len(os.listdir(cNextDir(n))) == 0:
+                os.rmdir(n)
+                print(f"{n} deleted")
+            else:
+                print(f"There may be some files inside")
     else:
-        print("That folder doesnt exist (maybe you got the name wrong?)")
+        print(f"{n} doesnt exist (maybe you got the name wrong?)")
 
-def wc(item):
-    x = item.split(" ", 1)
-    x = x[1]
-    os.system("wc {}".format(x))
-
-def findext(item):
-    g = item.split(" ", 1)
-    g = g[1]
-    i = list(os.listdir())
-    l = []
-    for x in i:
-        if x.endswith(g):
+def findext(item): # finds files with given extension
+    def ext(i):
+        if x.endswith(i):
             l.append(x)
-    print(l)
+    g = item.split(" ", 1)
+    if len(g) == 2:
+        g = g[1]
+        o = list(os.listdir())
+        l = []
+        if g[0] == ".":
+            for x in o:
+                ext(x)
+        else:
+            j = "." + g
+            for x in o:
+                ext(j)
+        print(l)
+
 
 
 os.chdir("/home")
 a = str(input(f"{os.getcwd()}/ "))
 while a!= "exit":
     x = a.split()
-    if x[0] == "cd":
+    if not x:
+        a = str(input(f"{os.getcwd()}/ "))
+        continue
+    elif x[0] == "cd": #works
         cd(a)
-    elif x[0] == "ls":
+    elif x[0] == "ls": # works
         ls()
-    elif x[0] == "run":
+    elif x[0] == "run": #works?
         run(a)
-    elif x[0] == "touch":
+    elif x[0] == "touch": #works
         touch(a)
     elif x[0] == "rm":
         rm(a)
